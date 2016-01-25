3b4f7135a633be88ea47944702a2791f86495e47
Джон <kolesnik-vlad@mail.ru>
2016-01-25 04:47:51 +0000

    Adding early stopping with 100 epoch grid search.

--- StochasticGradientDescentLearner.scala
+++ StochasticGradientDescentLearner.scala
@@ -7,30 +7,29 @@ import scala.util.control.Breaks
  */
 object StochasticGradientDescentLearner extends App {
 
+
   def apply(model: Model, corpus: String, maxEpochs: Int = 10, learningRate: Double, epochHook: (Int, Double, Model) => Unit): Unit = {
-    val loop = new Breaks
+    var previousDevAcc = 0.0
+    val epoch_loop = new Breaks
     val iterations = SentimentAnalysisCorpus.numExamples(corpus)
-    loop.breakable {
+    epoch_loop.breakable {
       for (i <- 0 until maxEpochs) {
         var accLoss = 0.0
         for (j <- 0 until iterations) {
           if (j % 1000 == 0) print(s"Iter $j\r")
-          // θ = θ − α∇θE[J(θ)]
           val (sentence, target) = SentimentAnalysisCorpus.getExample(corpus)
           val modelLoss = model.loss(sentence, target)
+//          println("modelLoss sim ="+model.vectorParams("param_w").dim)
           accLoss += modelLoss.forward()
           modelLoss.backward()
           modelLoss.update(learningRate) // updates parameters of the block
         }
         epochHook(i, accLoss, model)
-        if (hasExplodingGradient(model)) loop.break()
+        val modelValidationAcc = 100 * Evaluator(model, Main.validationSetName)
+        // early stopping
+        if (modelValidationAcc < previousDevAcc) epoch_loop.break()
+        else previousDevAcc = modelValidationAcc
       }
     }
-  }
-
-  def hasExplodingGradient(model: Model): Boolean = {
-    val (sentence, target) = SentimentAnalysisCorpus.getExample("train")
-    val modelLoss = model.loss(sentence, target)
-    modelLoss.forward().isNaN
   }
 }

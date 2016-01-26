b264d7d0cf29f32b320fcdf5ae4b2f231cb86655
Джон <kolesnik-vlad@mail.ru>
2016-01-26 20:19:21 +0000

    Changed Grid Search to early stop when loss on the validation set is stop decreasing. You can also changed to frequency of hook function with loss calculation being called.

--- Evaluator.scala
+++ Evaluator.scala
@@ -4,14 +4,19 @@ package uk.ac.ucl.cs.mr.statnlpbook.assignment3
  * @author rockt
  */
 object Evaluator {
-  def apply(model: Model, corpus: String): Double = {
+  def apply(model: Model, corpus: String): (Double,Double) = {
+    var accLoss = 0.0
     val total = SentimentAnalysisCorpus.numExamples(corpus)
     var correct = 0.0
     for (i <- 0 until total) {
       val (sentence, target) = SentimentAnalysisCorpus.getExample(corpus)
       val predict = model.predict(sentence)
       if (target == predict) correct = correct + 1
+
+      val modelLoss = model.loss(sentence, target)
+      accLoss += modelLoss.forward()
+//      println(accLoss)
     }
-    correct / total
+    (100 * (correct / total), accLoss)
   }
 }
